0c1b2d5adebc5160d9bbae034246173f9f1da329
jomla97 <jomla97@gmail.com>
2020-09-16 17:17:11 +0200

    Errorf fix

--- errorf.go
+++ errorf.go
@@ -6,6 +6,6 @@ import (
 )
 
 //Errorf writes an error to the log with a timestamp and [ERROR] tag appended with the specified formatting options
-func Errorf(format string, err error) {
-	fmt.Println("["+time.Now().Format(time.UnixDate)+"]", "\u001b[31m[ERROR]\u001b[0m", fmt.Errorf(format, err))
+func Errorf(format string, args ...interface{}) {
+	fmt.Println("["+time.Now().Format(time.UnixDate)+"]", "\u001b[31m[ERROR]\u001b[0m", fmt.Sprintf(format, args...))
 }
